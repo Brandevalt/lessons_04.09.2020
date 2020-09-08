688d098471c0f0b0c967f846448dde2ca7612898
abb <abb@step.lv>
2020-09-08 19:32:51 +0300

    q

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function () {
       id: 1,
       name: "Apple",
       img: "./image/apple.png",
-      counry: "Ukraine",
+      country: "Ukraine",
       weight: 20,
       price: 1,
     },
@@ -13,7 +13,7 @@ $(function () {
       id: 2,
       name: "Orange",
       img: "./image/orange.png",
-      counry: "Azerbaijan",
+      country: "Azerbaijan",
       weight: 20,
       price: 2,
     },
@@ -21,7 +21,7 @@ $(function () {
       id: 3,
       name: "Pineapple",
       img: "./image/pineapple.png",
-      counry: "India",
+      country: "India",
       weight: 19,
       price: 1.3,
     },
@@ -29,7 +29,7 @@ $(function () {
       id: 4,
       name: "Pear",
       img: "./image/pear.png",
-      counry: "Ukraine",
+      country: "Ukraine",
       weight: 29,
       price: 1.5,
     },
@@ -37,7 +37,7 @@ $(function () {
       id: 5,
       name: "Banana",
       img: "./image/banana.png",
-      counry: "India",
+      country: "India",
       weight: 23,
       price: 1.8,
     },
@@ -45,7 +45,7 @@ $(function () {
       id: 6,
       name: "Cherry",
       img: "./image/cherry.png",
-      counry: "Ukraine",
+      country: "Ukraine",
       weight: 14,
       price: 2.3,
     },
@@ -53,7 +53,7 @@ $(function () {
       id: 7,
       name: "Pomegranate",
       img: "./image/pomegranate.png",
-      counry: "Azerbaijan",
+      country: "Azerbaijan",
       weight: 34,
       price: 2,
     },
@@ -61,37 +61,25 @@ $(function () {
       id: 8,
       name: "Mango",
       img: "./image/mango.png",
-      counry: "India",
+      country: "India",
       weight: 34,
       price: 6.5,
     },
   ]
-
-  // $.getJSON("./js/product.json", function (data) {
-  //   $.each(data, function (key, val) {
-  //     console.log(val)
-  //   })
-  // })
-
-  // $.getJSON("./js/product.json", {
-  //   header: "Access-Control-Allow-Origin: *",
-  // }).done(function (data) {
-  //   $.each(data.items, function (i, item) {
-  //     console.log(item)
-  //   })
-  // })
-
+// local storage
   let fruitsJSON = JSON.stringify(arrFruits)
   localStorage.setItem("fruits", fruitsJSON)
-
   const getFruits = JSON.parse(localStorage.getItem("fruits"))
 
+
+
+// Yura
   getFruits.length > 0 &&
     getFruits.map((item, i) => {
       fruit.push(`<tr><td>${i}</td><td>${item.name}</td><td>
         <img src="${item.img}" alt="${item.name}" />
       </td>
-      <td>${item.counry}</td>
+      <td>${item.country}</td>
       <td>${item.weight} kg</td>
       <td>${item.price} $</td>
       <td class="edit">
@@ -102,12 +90,42 @@ $(function () {
       </td>
     </tr>`)
     })
-
   $("table tbody#fruit-list").append(fruit)
 
-  $(".edit").on("click", "img", function () {
-    let id = $(this).data("id")
 
-    console.log(getFruits[id])
+
+  $(".edit").one("click", "img", function () {
+    let id = $(this).data("id")
+   let ourElement = getFruits[id]
+  $('.modal-edit form').append(`
+  <div class="item">
+    <label for="title">Title: </label>
+    <input type="text" id="title" required value="${ourElement.name}">
+  </div>
+  <div class="item">
+    <label for="icon">Icon url: </label>
+    <input type="text" id="icon" required value="${ourElement.img}">
+  </div>
+  <div class="item">
+    <label for="country">Country: </label>
+    <input type="text" id="country" required value="${ourElement.country}">
+  </div>
+  <div class="item">
+    <label for="quantity">Quantity: </label>
+    <input type="number" id="quantity" required min="0" value="${ourElement.weight}">
+  </div>
+  <div class="item">
+    <label for="price">Price: </label>  
+    <input type="number" id="price" required min="0" value="${ourElement.price}">
+  </div>
+  <input type="submit" class="btn" value="Save" />
+`)
+
+    $('.modal-edit').css({display: 'block'}).hide(0).show(1000)
+
   })
 })
+
+
+// 
+
